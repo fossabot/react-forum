a67e7be82805154d3778a6d42b94c53b8f960e2d
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2018-02-18 11:30:22 -0800

    configured webpack a little further

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve('dist'),
+    path: __dirname + '/dist',
     filename: 'bundle.js',
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+  })],
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
